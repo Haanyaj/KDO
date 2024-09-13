6808a2c874f377a4bdaebf6313ea3bc35810cee5
Alvyn YANG <alvyn.yang@epitech.eu>
2024-09-13 16:52:10 +0200

    Updates

--- src/component/home.jsx
+++ src/component/home.jsx
@@ -1,60 +1,57 @@
-import React, { useState, useRef, useEffect } from 'react';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
+import React, { useState, useRef, useEffect } from "react";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 import {
   CssBaseline,
   AppBar,
   Toolbar,
   Typography,
   Container,
-  Grid,
   Button,
   Box,
   useScrollTrigger,
   Fab,
   Zoom,
   useMediaQuery,
-  IconButton,
-  Drawer,
   List,
   ListItem,
+  Grid,
   ListItemText,
-} from '@mui/material';
-import { ChevronRight, Menu as MenuIcon } from 'lucide-react';
+} from "@mui/material";
 
 // Création du thème
 const theme = createTheme({
   palette: {
     primary: {
-      main: '#000000', // Noir
+      main: "#000000", // Noir
     },
     secondary: {
-      main: '#74BBFF', // Bleu clair
+      main: "#74BBFF", // Bleu clair
     },
     background: {
-      default: '#000000', // Fond noir
+      default: "#000000", // Fond noir
     },
     text: {
-      primary: '#FFFFFF', // Texte blanc
+      primary: "#FFFFFF", // Texte blanc
     },
   },
   typography: {
     fontFamily: '"Arial", sans-serif',
     h1: {
       fontWeight: 700,
-      fontSize: '4rem',
-      '@media (max-width:600px)': {
-        fontSize: '2.5rem',
+      fontSize: "4rem",
+      "@media (max-width:600px)": {
+        fontSize: "2.5rem",
       },
     },
     h2: {
       fontWeight: 600,
-      fontSize: '2.5rem',
-      '@media (max-width:600px)': {
-        fontSize: '2rem',
+      fontSize: "2.5rem",
+      "@media (max-width:600px)": {
+        fontSize: "2rem",
       },
     },
     button: {
-      textTransform: 'none',
+      textTransform: "none",
     },
   },
   components: {
@@ -62,15 +59,15 @@ const theme = createTheme({
       styleOverrides: {
         root: {
           borderRadius: 20,
-          padding: '10px 20px',
+          padding: "10px 20px",
         },
       },
     },
     MuiAppBar: {
       styleOverrides: {
         root: {
-          backgroundColor: 'rgba(0, 0, 0, 0.7)',
-          boxShadow: 'none',
+          backgroundColor: "rgba(0, 0, 0, 0.7)",
+          boxShadow: "none",
         },
       },
     },
@@ -79,58 +76,39 @@ const theme = createTheme({
 
 // Sections du menu
 const sections = [
-  'Capture à 360°',
-  'Édition par l\'IA',
-  'Verrouillage de l\'horizon à 360°',
-  'Étanche',
-  'Facile à utiliser',
-  'Soyez au cœur de l\'action',
+  "Capture à 360°",
+  "CamCam et les dogs",
+  "Verrouillage de l'horizon à 360°",
 ];
 
 // Lien PayPal
 const paypalLink = "https://www.paypal.me/alvynyang?locale.x=en_FR";
 
-// Composant pour le bouton de retour en haut
-function ScrollTop(props) {
-  const { children } = props;
-  const trigger = useScrollTrigger({
-    disableHysteresis: true,
-    threshold: 100,
-  });
 
-  const handleClick = (event) => {
-    const anchor = (event.target.ownerDocument || document).querySelector('#back-to-top-anchor');
-    if (anchor) {
-      anchor.scrollIntoView({ behavior: 'smooth', block: 'center' });
-    }
-  };
-
-  return (
-    <Zoom in={trigger}>
-      <Box
-        onClick={handleClick}
-        role="presentation"
-        sx={{ position: 'fixed', bottom: 16, right: 16 }}
-      >
-        {children}
-      </Box>
-    </Zoom>
-  );
-}
 
 // Composant principal
 const Home = () => {
   const heroVideoRef = useRef(null);
-  const [selectedSection, setSelectedSection] = useState('Capture à 360°');
+  const camcamVideoRef = useRef(null);
+  const horizonLockVideoRef = useRef(null);
+  const [selectedSection, setSelectedSection] = useState("Capture à 360°");
   const [mobileOpen, setMobileOpen] = useState(false);
-  const isMobile = useMediaQuery(theme.breakpoints.down('md'));
+  const isMobile = useMediaQuery(theme.breakpoints.down("md"));
 
   useEffect(() => {
-    if (heroVideoRef.current) {
-      heroVideoRef.current.play().catch(error => {
-        console.error("Erreur de lecture automatique de la vidéo hero :", error);
-      });
-    }
+    const playVideo = async (videoRef) => {
+      if (videoRef.current) {
+        try {
+          await videoRef.current.play();
+        } catch (error) {
+          console.error("Erreur de lecture automatique de la vidéo :", error);
+        }
+      }
+    };
+
+    playVideo(heroVideoRef);
+    playVideo(camcamVideoRef);
+    playVideo(horizonLockVideoRef);
   }, []);
 
   const handleDrawerToggle = () => {
@@ -139,9 +117,11 @@ const Home = () => {
 
   const handleSectionClick = (section) => {
     setSelectedSection(section);
-    const element = document.getElementById(section.replace(/\s+/g, '-').toLowerCase());
+    const element = document.getElementById(
+      section.replace(/\s+/g, "-").toLowerCase()
+    );
     if (element) {
-      element.scrollIntoView({ behavior: 'smooth' });
+      element.scrollIntoView({ behavior: "smooth" });
     }
     if (isMobile) {
       setMobileOpen(false);
@@ -149,10 +129,14 @@ const Home = () => {
   };
 
   const drawer = (
-    <Box onClick={handleDrawerToggle} sx={{ textAlign: 'center' }}>
+    <Box onClick={handleDrawerToggle} sx={{ textAlign: "center" }}>
       <List>
         {sections.map((section) => (
-          <ListItem button key={section} onClick={() => handleSectionClick(section)}>
+          <ListItem
+            button
+            key={section}
+            onClick={() => handleSectionClick(section)}
+          >
             <ListItemText primary={section} />
           </ListItem>
         ))}
@@ -163,20 +147,20 @@ const Home = () => {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      
+
       {/* Barre de navigation */}
       <AppBar position="fixed">
-        <Toolbar sx={{ justifyContent: 'space-between' }}>
-          <Typography variant="h6" component="div" sx={{ fontWeight: 'bold' }}>
+        <Toolbar sx={{ justifyContent: "space-between" }}>
+          <Typography variant="h6" component="div" sx={{ fontWeight: "bold" }}>
             Insta360 X4
           </Typography>
-          <Box sx={{ display: 'flex', alignItems: 'center' }}>
+          <Box sx={{ display: "flex", alignItems: "center" }}>
             {!isMobile && (
-              <Box sx={{ display: 'flex' }}>
+              <Box sx={{ display: "flex" }}>
                 {sections.map((section) => (
-                  <Button 
+                  <Button
                     key={section}
-                    color="inherit" 
+                    color="inherit"
                     onClick={() => handleSectionClick(section)}
                   >
                     {section}
@@ -184,56 +168,56 @@ const Home = () => {
                 ))}
               </Box>
             )}
-            <Button 
-              variant="contained" 
-              color="secondary" 
-              sx={{ 
-                ml: 2, 
-                color: 'black', 
-                fontWeight: 'bold',
-                fontSize: isMobile ? '0.7rem' : '0.875rem',
-                padding: isMobile ? '5px 10px' : '10px 20px',
+            <Button
+              variant="contained"
+              color="secondary"
+              sx={{
+                ml: 2,
+                color: "black",
+                fontWeight: "bold",
+                fontSize: isMobile ? "0.7rem" : "0.875rem",
+                padding: isMobile ? "5px 10px" : "10px 20px",
               }}
               href={paypalLink}
               target="_blank"
               rel="noopener noreferrer"
             >
-              ACHETER MAINTENANT
+              COTISER POUR CAMCAM
             </Button>
-           
           </Box>
         </Toolbar>
       </AppBar>
-      
-     
 
       {/* Menu latéral vertical pour desktop */}
       {!isMobile && (
         <Box
           sx={{
-            position: 'fixed',
+            position: "fixed",
             left: 0,
-            top: '50%',
-            transform: 'translateY(-50%)',
+            top: "50%",
+            transform: "translateY(-50%)",
             zIndex: 1000,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'flex-start',
-            padding: '20px',
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "flex-start",
+            padding: "20px",
           }}
         >
           {sections.map((section, index) => (
             <Box
               key={index}
               sx={{
-                writingMode: 'vertical-rl',
-                textOrientation: 'mixed',
-                transform: 'rotate(180deg)',
-                marginBottom: '20px',
-                cursor: 'pointer',
-                color: selectedSection === section ? theme.palette.secondary.main : 'rgba(255, 255, 255, 0.7)',
-                transition: 'color 0.3s',
-                '&:hover': {
+                writingMode: "vertical-rl",
+                textOrientation: "mixed",
+                transform: "rotate(180deg)",
+                marginBottom: "20px",
+                cursor: "pointer",
+                color:
+                  selectedSection === section
+                    ? theme.palette.secondary.main
+                    : "rgba(255, 255, 255, 0.7)",
+                transition: "color 0.3s",
+                "&:hover": {
                   color: theme.palette.secondary.main,
                 },
               }}
@@ -244,10 +228,13 @@ const Home = () => {
           ))}
         </Box>
       )}
-      
+
       <Container maxWidth={false} disableGutters>
         {/* Section Capture à 360° */}
-        <Box id="capture-à-360°" sx={{ position: 'relative', height: '100vh', overflow: 'hidden' }}>
+        <Box
+          id="capture-à-360°"
+          sx={{ position: "relative", height: "100vh", overflow: "hidden" }}
+        >
           <video
             ref={heroVideoRef}
             preload="none"
@@ -256,83 +243,247 @@ const Home = () => {
             loop
             poster="https://res.insta360.com/static/016d26c7b543ee71cdbf277384bdf892/360-CG-PC.jpg"
             style={{
-              width: '100%',
-              height: '100%',
-              objectFit: 'cover',
-              position: 'absolute',
+              width: "100%",
+              height: "100%",
+              objectFit: "cover",
+              position: "absolute",
               top: 0,
               left: 0,
             }}
           >
-            <source src="https://media.insta360.com/static/1728bde3af4d712d58c03974cfb760de/360-CG-PC.mp4" type="video/mp4" />
+            <source
+              src="https://media.insta360.com/static/1728bde3af4d712d58c03974cfb760de/360-CG-PC.mp4"
+              type="video/mp4"
+            />
             Votre navigateur ne supporte pas la balise vidéo.
           </video>
           <Box
             sx={{
-              position: 'absolute',
-              top: '50%',
-              left: '50%',
-              transform: 'translate(-50%, -50%)',
-              textAlign: 'center',
-              color: 'white',
+              position: "absolute",
+              top: "50%",
+              left: "50%",
+              transform: "translate(-50%, -50%)",
+              textAlign: "center",
+              color: "white",
               zIndex: 1,
-              width: '90%',
+              width: "90%",
             }}
           >
-            <Typography variant="h1" gutterBottom sx={{ color: theme.palette.secondary.main }}>
+            <Typography
+              variant="h1"
+              gutterBottom
+              sx={{ color: theme.palette.secondary.main }}
+            >
               Capture à 360°
             </Typography>
-            <Typography variant="h4" gutterBottom sx={{ fontSize: { xs: '1.5rem', md: '2.125rem' } }}>
-              Capturez l'instant, créez des souvenirs
+            <Typography
+              variant="h4"
+              gutterBottom
+              sx={{ fontSize: { xs: "1.5rem", md: "2.125rem" } }}
+            >
+              Capturez l'instant BG, créez des souvenirs de fou furieux
             </Typography>
-            <Button 
-              variant="contained" 
-              color="secondary" 
-              size="large" 
-              sx={{ 
-                color: 'black', 
-                fontWeight: 'bold',
-                fontSize: isMobile ? '0.8rem' : '1rem',
-                padding: isMobile ? '8px 16px' : '10px 20px',
+            <Button
+              variant="contained"
+              color="secondary"
+              size="large"
+              sx={{
+                color: "black",
+                fontWeight: "bold",
+                fontSize: isMobile ? "0.8rem" : "1rem",
+                padding: isMobile ? "8px 16px" : "10px 20px",
               }}
               href={paypalLink}
               target="_blank"
               rel="noopener noreferrer"
             >
-              Découvrir
+              Cotiser pour CamCam
             </Button>
           </Box>
         </Box>
-        
-        {/* Autres sections */}
-        {sections.slice(1).map((section, index) => (
-          <Box 
-            key={section}
-            id={section.replace(/\s+/g, '-').toLowerCase()} 
-            sx={{ 
-              bgcolor: index % 2 === 0 ? 'black' : 'rgba(255, 255, 255, 0.05)', 
-              color: 'white', 
-              py: 8 
-            }}
-          >
-            <Container maxWidth="lg">
-              <Typography variant="h2" gutterBottom align="center">
-                {section}
-              </Typography>
-              <Typography variant="body1" paragraph>
-                Contenu pour la section {section}. Ajoutez ici plus de détails sur cette fonctionnalité.
-              </Typography>
-            </Container>
-          </Box>
-        ))}
+
+        {/* Section CamCam et les dogs */}
+        <Box
+          id="camcam-et-les-dogs"
+          sx={{
+            bgcolor: "black",
+            color: "white",
+            py: 8,
+          }}
+        >
+          <Container maxWidth="lg">
+            <Typography
+              variant="h6"
+              gutterBottom
+              sx={{ color: theme.palette.secondary.main }}
+            >
+              CAMCAM ET LES DOGS
+            </Typography>
+            <Typography variant="h2" gutterBottom sx={{ fontWeight: "bold" }}>
+              PERFORMANCE DE FOU SA MERE !
+            </Typography>
+
+            <Box
+              sx={{
+                position: "relative",
+                height: "40vh",
+                overflow: "hidden",
+                borderRadius: "20px",
+              }}
+            >
+              <video
+                ref={camcamVideoRef}
+                preload="none"
+                playsInline
+                muted
+                loop
+                poster="https://res.insta360.com/static/a2a2f124a31a5b2b5b3329591ec590b9/Stabilization.jpg"
+                style={{
+                  width: "100%",
+                  height: "100%",
+                  objectFit: "cover",
+                  borderRadius: "20px",
+                }}
+              >
+                <source
+                  src="https://media.insta360.com/static/151b4947ca8ccb546e214985de506ee7/Stabilization.mp4"
+                  type="video/mp4"
+                />
+                Votre navigateur ne supporte pas la balise vidéo.
+              </video>
+            </Box>
+          </Container>
+        </Box>
+
+        {/* Section Verrouillage de l'horizon à 360° */}
+        <Box
+          id="verrouillage-de-l'horizon-à-360°"
+          sx={{
+            bgcolor: "black",
+            color: "white",
+            py: 8,
+          }}
+        >
+          <Container maxWidth="lg">
+            <Typography
+              variant="h6"
+              gutterBottom
+              sx={{ color: theme.palette.secondary.main }}
+            >
+              VERROUILLAGE DE L'HORIZON À 360°
+            </Typography>
+            <Typography variant="h2" gutterBottom sx={{ fontWeight: "bold" }}>
+              LA STABILITÉ ELLE TUE A MOORT !
+            </Typography>
+
+            <Box
+              sx={{
+                position: "relative",
+                height: "50vh",
+                overflow: "hidden",
+                borderRadius: "20px",
+              }}
+            >
+              <video
+                ref={horizonLockVideoRef}
+                preload="none"
+                playsInline
+                muted
+                loop
+                poster="https://res.insta360.com/static/6facb8d01fe6c8c0323295794daaec95/Bullet-Time_PC.jpg"
+                style={{
+                  width: "80%",
+                  height: "100%",
+                  objectFit: "cover",
+                  borderRadius: "20px",
+                }}
+              >
+                <source
+                  src="https://media.insta360.com/static/2ecd9bd98653d44f80e80749fa308f05/Bullet%20Time_PC_1728x972.mp4"
+                  type="video/mp4"
+                />
+                Votre navigateur ne supporte pas la balise vidéo.
+              </video>
+            </Box>
+          </Container>
+        </Box>
+
+ 
+      {/* Nouvelle section d'achat */}
+      <Box
+  id="acheter-maintenant"
+  sx={{
+    bgcolor: '#000',
+    color: 'white',
+    position: 'relative',
+    overflow: 'hidden',
+    height: '100vh',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundImage: { xs: '', md: 'none' },
+  }}
+>
+  <Container maxWidth="lg" sx={{ position: 'relative', zIndex: 2 }}>
+    <Grid container spacing={4} alignItems="center">
+      <Grid item xs={12} md={6} sx={{ textAlign: { xs: 'center', md: 'left' } }}>
+        <Typography variant="h2" component="h2" gutterBottom sx={{ 
+          fontWeight: 'bold',
+          fontSize: { xs: '2rem', md: '3rem' }
+        }}>
+          Insta360 X4
+        </Typography>
+        <Typography variant="h4" gutterBottom sx={{ 
+          fontWeight: 300,
+          fontSize: { xs: '1.2rem', md: '1.5rem' }
+        }}>
+          Filmez l'impossible. (Chui Chokbar)
+        </Typography>
+        <Button
+          variant="contained"
+          size="large"
+          href="https://www.paypal.me/alvynyang?locale.x=en_FR"
+          target="_blank"
+          rel="noopener noreferrer"
+          sx={{
+            mt: 3,
+            bgcolor: '#74BBFF',
+            color: 'black',
+            fontWeight: 'bold',
+            padding: { xs: '10px 30px', md: '10px 20px' },
+            borderRadius: { xs: '25px', md: '4px' },
+            textTransform: { xs: 'uppercase', md: 'none' },
+            '&:hover': {
+              bgcolor: '#5ba8f5',
+            },
+          }}
+        >
+          {({ xs: 'COTISER POUR CAMCAM', md: 'COTISER POUR CAMCAM' })[
+            Object.keys({ xs: 'COTISER POUR CAMCAM', md: 'COTISER POUR CAMCAM' })
+              .filter((size) => window.matchMedia(`(min-width: ${theme.breakpoints.values[size]}px)`).matches)
+              .pop()
+          ]}
+        </Button>
+      </Grid>
+    </Grid>
+  </Container>
+  <Box
+    component="img"
+    src="https://res.insta360.com/static/8e5f3048318cd127658a5205f2fe61fd/buy_page.jpg"
+    alt="Insta360 X4"
+    sx={{
+      position: { xs: 'absolute', md: 'absolute' },
+      top: { xs: '18%', md: '50%' },
+      right: { xs: '-20%', md: '20%' },
+      transform: { xs: 'none', md: 'translateY(-50%)' },
+      maxHeight: { xs: '120%', md: '90%' },
+      maxWidth: { xs: '214%', md: '60%' },
+      marginTop: { xs: '32px', md: 0 },
+      objectFit: 'contain',
+    }}
+  />
+</Box>
       </Container>
-      
-      {/* Bouton de retour en haut */}
-      <ScrollTop>
-        <Fab color="secondary" size="small" aria-label="scroll back to top">
-          <ChevronRight />
-        </Fab>
-      </ScrollTop>
     </ThemeProvider>
   );
 };
